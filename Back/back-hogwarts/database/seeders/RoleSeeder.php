565bbae6249eab9e7018520498f67f2255c18774
cynthiarivasmoral <cynthiarivasmoral@gmail.com>
2024-11-07 12:14:51 +0100

    mejora seeder

--- Back/back-hogwarts/database/seeders/RoleSeeder.php
+++ Back/back-hogwarts/database/seeders/RoleSeeder.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Role;
 
+
 class RoleSeeder extends Seeder
 {
 
     public function run(): void{
-        \App\Models\Role::factory(4)->create();
+        Role::factory(4)->create();
     }
 }
