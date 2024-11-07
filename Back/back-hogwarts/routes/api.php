fcd3088710282e31aa94301faa38317e9146c944
DoubleM <141600055+moonmdc@users.noreply.github.com>
2024-11-07 17:49:34 +0100

    Rutas de aldministrador

--- Back/back-hogwarts/routes/api.php
+++ Back/back-hogwarts/routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\AdminController;
+
+Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
+    Route::get('/users', [AdminController::class, 'index'])->middleware('admin');
+    Route::get('/user/{id}', [AdminController::class, 'show'])->middleware('admin');
+    Route::put('/user/{id}', [AdminController::class, 'update'])->middleware('admin');
+    Route::delete('/user/{id}', [AdminController::class, 'destroy'])->middleware('admin');
+    Route::post('/user/{id}', [AdminController::class, 'giveRole'])->middleware('admin');
+    Route::delete('/user/{id}', [AdminController::class, 'retireRole'])->middleware('admin');
+});
