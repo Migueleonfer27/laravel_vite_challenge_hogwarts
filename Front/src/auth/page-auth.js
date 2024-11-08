bbd81c9f8905ca8a01dc8bb940bd3295f94281ac
Migueleonfer27 <94015029+Migueleonfer27@users.noreply.github.com>
2024-11-08 13:42:05 +0100

    Bug animación lechuza

--- Front/src/auth/page-auth.js
+++ Front/src/auth/page-auth.js
@@ -1,40 +1,50 @@
 import { handleRegister, handleLogin, handleLogout } from "./auth-provider";
-import {getToken} from "../../storage/tokenManager";
+import { getToken } from "../../storage/tokenManager";
 
 // Miguel León Fernández
 export const initAuth = () => {
-    selectForm()
+    addOwl();
+    selectForm();
     toggleAuthButtons(getToken() !== null);
 }
 
 // Miguel León Fernández
-const addRegisterForm = () => {
+const addOwl = () => {
     const mainContent = document.querySelector('#main-container');
     mainContent.innerHTML = `
         <div class="container mt-5">
             <div class="row justify-content-center align-items-center">
-                <div class="col-md-6 d-none d-md-block">
+                <div class="col-md-6 d-none d-md-block owl-container">
                     <img src="./assets/img/lechuza.png" alt="Imagen de Registro" class="img-fluid animated-owl" width="75%">
                 </div>
-                <div class="col-md-6">
-                    <h3 class="text-center text-light mb-4">Registro de Alumno</h3>
-                    <form id="registerForm">
-                        <div class="mb-3">
-                            <label for="name" class="form-label text-light">Nombre</label>
-                            <input type="text" class="form-control" id="name" name="name" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="email" class="form-label text-light">Correo Electrónico</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label text-light">Contraseña</label>
-                            <input type="password" class="form-control" id="password" name="password" required minlength="6">
-                        </div>
-                        <button type="submit" class="btn btn-primary w-100">Registrar</button>
-                    </form>
+                <div class="col-md-6 form-container" id="form-container">
                 </div>
             </div>
+        </div>
+    `;
+}
+
+// Miguel León Fernández
+const addRegisterForm = () => {
+    const formContainer = document.querySelector('#form-container');
+    formContainer.innerHTML = `
+        <div class="bg-secondary p-5">
+            <h3 class="text-center text-light mb-4">Registro de Alumno</h3>
+            <form id="registerForm">
+                <div class="mb-3">
+                    <label for="name" class="form-label text-light">Nombre</label>
+                    <input type="text" class="form-control" id="name" name="name" required>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label text-light">Correo Electrónico</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label text-light">Contraseña</label>
+                    <input type="password" class="form-control" id="password" name="password" required minlength="6">
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Registrar</button>
+            </form>
         </div>
     `;
     handleRegister();
@@ -42,28 +52,24 @@ const addRegisterForm = () => {
 
 // Miguel León Fernández
 const addLoginForm = () => {
-    const mainContent = document.querySelector('#main-container');
-    mainContent.innerHTML = `
-        <div class="container mt-5">
-            <div class="row justify-content-center align-items-center">
-                <div class="col-md-6 d-none d-md-block">
-                    <img src="./assets/img/lechuza.png" alt="Imagen de Registro" class="img-fluid animated-owl" width="75%">
+    const formContainer = document.querySelector('#form-container');
+    formContainer.innerHTML = `
+        <div class="bg-secondary p-5">
+            <h3 class="text-center text-light mb-4">Inicio de sesión</h3>
+            <form id="loginForm">
+                <div class="mb-3">
+                    <label for="email" class="form-label text-light">Correo Electrónico</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
                 </div>
-                <div class="col-md-6">
-                    <h3 class="text-center text-light mb-4">Inicio de sesión</h3>
-                    <form id="loginForm">
-                        <div class="mb-3">
-                            <label for="email" class="form-label text-light">Correo Electrónico</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label text-light">Contraseña</label>
-                            <input type="password" class="form-control" id="password" name="password" required minlength="6">
-                        </div>
-                        <button type="submit" class="btn btn-primary w-100">Iniciar sesión</button>
-                    </form>
+                <div class="mb-3">
+                    <label for="password" class="form-label text-light">Contraseña</label>
+                    <input type="password" class="form-control" id="password" name="password" required minlength="6">
                 </div>
-            </div>
+                <div class="mt-3 text-center mb-2">
+                    <a href="email/email.html" class="text-light text-decoration-none">¿Has olvidado tu contraseña?</a>
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Iniciar sesión</button>
+            </form>
         </div>
     `;
     handleLogin();
