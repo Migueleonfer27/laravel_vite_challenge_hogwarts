9b5d74d4ae3aaf22e0f741d32828ccd5e48744ff
Migueleonfer27 <94015029+Migueleonfer27@users.noreply.github.com>
2024-11-09 20:26:06 +0100

    Pintar componentes nav, footer y estilos varios

--- Front/src/auth/page-auth.js
+++ Front/src/auth/page-auth.js
@@ -1,9 +1,13 @@
 import { handleRegister, handleLogin, handleLogout } from "./auth-provider";
 import { getToken } from "../../storage/tokenManager";
-import {loadPage} from "../js/router";
+import { loadPage } from "../js/router";
+import { buildHeader } from "../components/buildHeader";
+import { buildFooter } from "../components/buildFooter";
 
 // Miguel León Fernández
 export const initAuth = () => {
+    buildHeader();
+    buildFooter();
     addWelcome();
     addOwl();
     addLoginForm();
@@ -16,7 +20,7 @@ const addWelcome = () => {
     const mainContent = document.querySelector('#main-container');
     mainContent.innerHTML = `
     <div class="container mt-5">
-      <div class="card text-center text-light shadow-lg p-4 bg-secondary">
+      <div class="card text-center text-primary-person shadow-lg p-4 bg-cuaternary-person border-2 border-primary-person">
         <h1 class="card-title">¡Bienvenido a la Academia Howarts!</h1>
         <p class="card-text mt-3">Nos alegra darte la bienvenida a un lugar mágico donde podrás aprender y desarrollar tus habilidades. Prepárate para vivir experiencias inolvidables y descubrir todos los secretos que Howarts tiene para ofrecerte. ¡Que comience la aventura!</p>
       </div>
@@ -46,11 +50,11 @@ const addFormButtons = () => {
     if (!document.querySelector('#authBtn')) {
         const authBtnContainer = document.createElement('div');
         authBtnContainer.id = 'authBtn';
-        authBtnContainer.classList.add('d-flex', 'justify-content-evenly', 'ms-auto', 'bg-secondary');
+        authBtnContainer.classList.add('d-flex', 'justify-content-evenly', 'ms-auto');
 
         authBtnContainer.innerHTML = `
-            <button id="loginBtn" class="btn btn-danger w-50">Inicio de sesión</button>
-            <button id="registerBtn" class="btn btn-danger w-50">Registrarse</button>
+            <button id="loginBtn" class="btn btn-secondary w-50">Inicio de sesión</button>
+            <button id="registerBtn" class="btn btn-secondary w-50">Registrarse</button>
         `;
 
         formContainer.prepend(authBtnContainer);
@@ -62,22 +66,22 @@ const addFormButtons = () => {
 const addRegisterForm = () => {
     const formContainer = document.querySelector('#form-container');
     formContainer.innerHTML = `
-        <div class="bg-secondary p-5">
-            <h3 class="text-center text-light mb-4">Registro de Alumno</h3>
+        <div class="bg-cuaternary-person p-5">
+            <h3 class="text-center text-primary-person mb-4">Registro de Alumno</h3>
             <form id="registerForm">
                 <div class="mb-3">
-                    <label for="name" class="form-label text-light">Nombre</label>
-                    <input type="text" class="form-control" id="name" name="name" required>
+                    <label for="name" class="form-label text-primary-person">Nombre</label>
+                    <input type="text" class="form-control bg-primary-person" id="name" name="name" required>
                 </div>
                 <div class="mb-3">
-                    <label for="email" class="form-label text-light">Correo Electrónico</label>
-                    <input type="email" class="form-control" id="email" name="email" required>
+                    <label for="email" class="form-label text-primary-person">Correo Electrónico</label>
+                    <input type="email" class="form-control bg-primary-person" id="email" name="email" required>
                 </div>
                 <div class="mb-3">
-                    <label for="password" class="form-label text-light">Contraseña</label>
-                    <input type="password" class="form-control" id="password" name="password" required minlength="6">
+                    <label for="password" class="form-label text-primary-person">Contraseña</label>
+                    <input type="password" class="form-control bg-primary-person" id="password" name="password" required minlength="6">
                 </div>
-                <button type="submit" class="btn btn-primary w-100">Registrar</button>
+                <button type="submit" class="btn btn-secondary w-100">Registrar</button>
             </form>
         </div>
     `;
@@ -89,21 +93,21 @@ const addRegisterForm = () => {
 const addLoginForm = () => {
     const formContainer = document.querySelector('#form-container');
     formContainer.innerHTML = `
-        <div class="bg-secondary p-5">
-            <h3 class="text-center text-light mb-4">Inicio de sesión</h3>
+        <div class="bg-cuaternary-person p-5">
+            <h3 class="text-center text-primary-person mb-4">Inicio de sesión</h3>
             <form id="loginForm">
                 <div class="mb-3">
-                    <label for="email" class="form-label text-light">Correo Electrónico</label>
-                    <input type="email" class="form-control" id="email" name="email" required>
+                    <label for="email" class="form-label text-primary-person">Correo Electrónico</label>
+                    <input type="email" class="form-control bg-primary-person" id="email" name="email" required>
                 </div>
                 <div class="mb-3">
-                    <label for="password" class="form-label text-light">Contraseña</label>
-                    <input type="password" class="form-control" id="password" name="password" required minlength="6">
+                    <label for="password" class="form-label text-primary-person">Contraseña</label>
+                    <input type="password" class="form-control bg-primary-person" id="password" name="password" required minlength="6">
                 </div>
                 <div class="mt-3 text-center mb-2">
-                    <a route-link="/email" href="#" class="text-light text-decoration-none">¿Has olvidado tu contraseña?</a>
+                    <a route-link="/email" href="#" class="text-primary-person text-decoration-none">¿Has olvidado tu contraseña?</a>
                 </div>
-                <button type="submit" class="btn btn-primary w-100">Iniciar sesión</button>
+                <button type="submit" class="btn btn-secondary w-100">Iniciar sesión</button>
             </form>
         </div>
     `;
