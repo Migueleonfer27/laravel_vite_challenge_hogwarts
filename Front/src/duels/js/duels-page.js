e4e2872121556bebfefb75aeef328b4acbea9a5d
cynthiarivasmoral <cynthiarivasmoral@gmail.com>
2024-12-04 16:16:29 +0100

    cartas maquina

--- Front/src/duels/js/duels-page.js
+++ Front/src/duels/js/duels-page.js
@@ -6,25 +6,98 @@ import {getSpellLearned} from "../../spell/js/spell-provider";
 const buildBody = () => {
     const mainContent = document.querySelector('#main-container');
     mainContent.innerHTML = `
+        <div class="deck" id="deckMachine"></div>
         <div class="d-flex gap-4">
-            <div class="selection-area" id="selectionArea"></div>
-            <div class="selection-area" id="selectionArea"></div>
+            <div class="selection-area" id="selectionAreaUser"></div>
+            <div class="selection-area" id="selectionAreaMachine"></div>
         </div>
-        <div class="deck" id="deck"></div>
+        <div class="deck" id="deckUser"></div>
     `;
 }
 
-const buildDeck = () => {
+const efectSelectionMachine = (deckMachine) => {
+    const selectionAreaMachine = document.getElementById('selectionAreaMachine');
+    if(!selectionAreaMachine.querySelector('.card-down')){
+        const selectionMachineIndex = Math.floor(Math.random() * 5)
+        const cardsMachine = deckMachine.querySelectorAll('.card-down')
+        const cardSelectedMachine = cardsMachine[selectionMachineIndex]
+
+        // Obtener la posición inicial de la carta y la posición del destino
+        const cardRect = cardSelectedMachine.getBoundingClientRect();
+        const destRect = selectionAreaMachine.getBoundingClientRect();
+
+        // Calcular la distancia de movimiento
+        const deltaX = destRect.left - cardRect.left + 10;
+        const deltaY = destRect.top - cardRect.top + 10;
+
+        // Aplicar la animación (transformación)
+        cardSelectedMachine.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+
+        // Cuando la animación termine, mover realmente la carta al destino
+        cardSelectedMachine.addEventListener( 'transitionend',
+            () => {
+                // Resetear transformaciones y mover la carta al nuevo contenedor
+                cardSelectedMachine.style.transform = '';
+                selectionAreaMachine.appendChild(cardSelectedMachine)
+            },
+            { once: true }
+        );
+    }
+}
+
+const efectSelectionUser = (deckUser, cardSelectedUser) => {
+    const selectionAreaUser = document.getElementById('selectionAreaUser');
+    if(!selectionAreaUser.querySelector('.card-up')){
+
+        // Obtener la posición inicial de la carta y la posición del destino
+        const cardRect = cardSelectedUser.getBoundingClientRect();
+        const destRect = selectionAreaUser.getBoundingClientRect();
+
+        // Calcular la distancia de movimiento
+        const deltaX = destRect.left - cardRect.left + 10;
+        const deltaY = destRect.top - cardRect.top + 10;
+
+        // Aplicar la animación (transformación)
+        cardSelectedUser.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+
+        // Cuando la animación termine, mover realmente la carta al destino
+        cardSelectedUser.addEventListener( 'transitionend',
+            () => {
+                // Resetear transformaciones y mover la carta al nuevo contenedor
+                cardSelectedUser.style.transform = '';
+                selectionAreaUser.appendChild(cardSelectedUser)
+            },
+            { once: true }
+        );
+    }
+}
+
+const buildDecks = () => {
+    // Se genera el mazo de la maquina
+    const deckMachine = document.getElementById('deckMachine');
+    for (let i = 0; i < 5; i++) {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card-down');
+        cardElement.setAttribute('id', i);
+        cardElement.innerHTML = `
+                <div class="d-flex flex-column p-4">
+                    
+                </div>
+            `;
+        deckMachine.appendChild(cardElement);
+    }
+
+    // Se genera el mazo del Usuario
     getSpellLearned().then( response => {
         // Se recuperan los hechizos aprendidos por el usuario y se crean cartas dinamicamente
         const spellLearned = response['spell']
-        const deck = document.getElementById('deck');
-        const selectionArea = document.getElementById('selectionArea');
+        const deckUser = document.getElementById('deckUser');
+        const selectionAreaUser = document.getElementById('selectionAreaUser');
         let selectedCard = null;
 
         spellLearned.forEach(spell => {
             const cardElement = document.createElement('div');
-            cardElement.classList.add('card');
+            cardElement.classList.add('card-up');
             cardElement.setAttribute('id', spell.id);
             cardElement.setAttribute('data', JSON.stringify(spell));
             cardElement.innerHTML = `
@@ -40,23 +113,27 @@ const buildDeck = () => {
                 </div>
             `;
             cardElement.addEventListener("click", (e) => {
-                const card = e.target.closest('.card')
+                const card = e.target.closest('.card-up')
                 if(card){
                     // si la carta que se pincha esta dentro del mazo, se lleva del mazo a la zona de selección.
                     // Y, si no, la carta que se ha pinchado esta en la zona de selección y lo que se hace es retirarla al mazo
-                    if (card.parentNode === deck) {
+                    if (card.parentNode === deckUser) {
                         // solo puede haber una carta de hechizo seleccionada al mismo tiempo
                         if(!selectedCard){
-                            selectionArea.appendChild(card)
                             selectedCard = card
+                            efectSelectionUser(deckUser, card)
+
+                            // cuando se selecciona un hechizo se desencadena que la maquina elija el suyo (no se elige de
+                            // verdad en este momento, es una simulación, la elección se hara en back)
+                            setTimeout(efectSelectionMachine, 600, deckMachine);
                         }
                     }else{
-                        deck.appendChild(card)
+                        deckUser.appendChild(card)
                         selectedCard = null
                     }
                 }
             });
-            deck.appendChild(cardElement);
+            deckUser.appendChild(cardElement);
         });
 
         hideLoader()
@@ -69,4 +146,4 @@ buildLoader()
 buildHeader()
 buildFooter()
 buildBody()
-buildDeck()
+buildDecks()
